44520873305d92ed67961e5b1607723a648e6da1
Esis <jesisp17@gmail.com>
2025-10-28 01:32:00 -0300

    removed old sidebar

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,6 @@
-import { Sidebar } from "@/components/ui/sidebar";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen ">
-      <Sidebar />
       <main className="flex-1 sm:absolute sm:right-0 sm:top-0 sm:w-[calc(100%-250px)]">
         {children}
       </main>
