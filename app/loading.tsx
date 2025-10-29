8d0e9ed931094d9c5cae86fdfda4eeb2b01cd79c
Esis <jesisp17@gmail.com>
2025-10-29 18:43:06 -0300

    breaking change: upgraded to next 16

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,6 @@ export default function Loading() {
   // See: https://nextjs.org/docs/app/api-reference/file-conventions/loading
   return (
     <div className="flex items-center justify-center h-screen">
-      {/* From Uiverse.io by Nawsome */}
       <div className="boxes">
         <div className="box">
           <div></div>
--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { stackServerApp } from "@/stack/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const user = await stackServerApp.getUser();
   const { pathname } = request.nextUrl;
   const loggedOffRoutes = ["/handler/sign-in", "/handler/sign-up", "/"];
